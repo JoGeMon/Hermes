c2273900c6c6129f0cfb66c88d5c339e1c7feead
JoGeMon <the.white.tiger@gmail.com>
2015-06-05 14:10:52 -0300

    carga viernes

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -7,63 +7,81 @@
 	    <title>Ematel - Gestión</title>
 	    {{ HTML::script('assets/js/jquery.js'); }}
 		{{ HTML::script('assets/js/jquery-ui.min.js'); }}
-		{{ HTML::style('assets/js/slidebars.min.js'); }}		
+		{{ HTML::script('assets/js/slidebars.min.js'); }}		
 		{{ HTML::script('assets/js/jquery.dataTables.js'); }}
 		{{ HTML::script('assets/js/bootstrap.js'); }}
 		{{ HTML::style('assets/css/bootstrap.min.css'); }}
 		{{ HTML::style('assets/css/slidebars.min.css'); }}
 		{{ HTML::style('assets/css/dataTables.css'); }}
 		{{ HTML::style('assets/css/jquery-ui.css'); }}
+		<style type="text/css">
+			#sidebar{
+				background-color: #398ab9;
+				min-height: 1451px;
+			}
+
+			#header{
+				background-color: #398ab9;
+				min-height: 
+			}
+
+			#menu > li > a{
+				color: #FFF;
+			}
+
+			.logo{
+				width: 170px;
+				margin: 25px;
+				display: block;
+				text-align: center;
+			}
+		</style>
 	</head>
-	<style type="text/css">
-		#sidebar{
-			background-color: #398ab9;
-			min-height: 1451px;
-		}
-
-		#menu > li > a{
-			color: #FFF;
-		}
-	</style>
-	<script type="text/javascript">
-		$(document).ready(function(){
-			var lateral = new $.slidebars();
-			//$.slidebars();
-			$('btnShow').on('click',function(){
-				lateral.slidebars.toggle('left');
-			});
-		});
-	</script>
 	<body>
-		<div id="sb-site" class="container">
-    		<div class="col-md-12">
-				<nav class="navbar navbar-default" role="navigation">
-					<img class="img-rounded" src="{{asset('assets/images/cabecera.jpg')}}" width="100%">
-				      	<div class="collapse navbar-collapse navbar-ex1-collapse">
-			              	<ul class="nav navbar-nav">
-		                   	<li><a href="#">Mision y visión</a></li>
-		                   	<li><a href="#">FAQ</a></li>
-		               	</ul>
-		           	</div>
-				</nav><!--<nav class="navbar navbar-default" role="navigation">-->
-				<button class="btn btn-primry" id="btnShow">Link</button>
-	            @yield('contenido', 'Default Content')
-	            <hr/>
-		        <footer align="center">
-					<h4>Hermes - Ematel S.A</h4>
-		        </footer>
-			</div><!-- <div class="col-md-12">-->
-		</div><!-- <div id="sb-site" class="container"> -->
-
 		<div class="col-md-2 sb-slidebar sb-left" id="sidebar" role="navigation">
-	       	<ul id="menu">
-	          	<li><a href="#"><i class="glyphicon glyphicon-bullhorn"></i> Noticias</a></li>
-	           	<li><a href="#"><i class="glyphicon glyphicon-barcode"></i> SIG</a></li>
+			<a href="#" class="logo text-center" >
+				<img src="{{asset('assets/images/user_vacio.jpg')}}" class="img-responsive img-circle center-block" width="100px" height="100" alt="logo">
+				<p style="color:#FFF">Usuario</p>
+			</a>
+			<ul id="menu">
+				<li><a href="#opt1" data-toggle="collapse" aria-controls="opt1"><i class="glyphicon glyphicon-bullhorn"></i> Noticias +</a></li>
+					<ul class="collapse" id="opt1">
+						<li>pimera</li>
+						<li>segunda</li>
+						<li>tercera</li>
+					</ul>
+				<li><a href="#"><i class="glyphicon glyphicon-barcode"></i> SIG</a></li>
 				<li><a href="capacitacion.php"><i class="glyphicon glyphicon-book"></i> Capacitación <span class="label label-warning">1</span></a></li>
 				<li><a href="#"><i class="glyphicon glyphicon-dashboard"></i> Panel de seguimiento <span class="label label-danger"><i class="glyphicon glyphicon-exclamation-sign"></i></span></a></li>
 				<hr>
 				<li><a href="administracion.php"><i class="glyphicon glyphicon-cog"></i> Administrar</a></li>
 			</ul>
-		</div><!-- Sidebar -->
+		</div><!-- Sidebar -->		
+		<div id="sb-site" class="container">
+			<div class="row">
+				<div class="col-md-12" id="header">
+					<nav class="navbar navbar-default" role="navigation">
+						<img class="img-rounded" src="{{asset('assets/images/cabecera.jpg')}}" width="100%">
+					</nav><!--<nav class="navbar navbar-default" role="navigation">-->
+				</div>
+			</div>
+			<div class="row">
+				
+	    		<div class="col-md-10">
+					<button class="btn btn-primry sb-toggle-left" id="btnShow">Link</button>
+		            @yield('contenido', 'Default Content')
+		            <hr/>
+			        <footer align="center">
+						<h4>Hermes - Ematel S.A</h4>
+			        </footer>
+				</div><!-- <div class="col-md-12">-->
+			</div>
+		</div><!-- <div id="sb-site" class="container"> -->
+		<script type="text/javascript">
+			$(document).ready(function() {
+				// Initiate Slidebars
+				$.slidebars();
+			});
+		</script>
 	</body>
 </html>
